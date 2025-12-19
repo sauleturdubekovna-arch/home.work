b6472fc5d949baf62873a8e3a36a1fd9ddbfc81d
sauleturdubekovna-arch <sauleturdubekovna@gmail.com>
2025-12-19 22:53:18 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+const left = document.getElementById("left");
+left.style.display = "flex";
+left.style.flexDirection = "column";
+left.style.gap = "20px";
+
+const blue = document.getElementById("blue");
+blue.style.width = "250px";
+blue.style.height = "80px";
+blue.style.backgroundColor = "blue";
+blue.style.borderRadius = "20px";
+
+const yellow = document.getElementById("yellow");
+yellow.style.width = "90px";
+yellow.style.height = "90px";
+yellow.style.backgroundColor = "orange";
+yellow.style.borderRadius = "15px";
+
+const red = document.getElementById("red");
+red.style.width = "100px";
+red.style.height = "100px";
+red.style.backgroundColor = "red";
+red.style.borderRadius = "50%";
+
+const img = document.getElementById("img");
+img.style.width = "380px";
+img.style.height = "220px";
+img.style.display = "flex";
+img.style.alignItems = "center";
+img.style.justifyContent = "center";
+img.style.borderRadius = "15px";
+
+
+const picture = document.getElementById("picture");
+picture.style.maxWidth = "90%";
+picture.style.maxHeight = "90%";
+picture.style.borderRadius = "10px";
+
